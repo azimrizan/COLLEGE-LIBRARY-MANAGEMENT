ea620b7015533f551dae78e884031ddfed1c0b67
unknown <azimrizan9@gmail.com>
2023-07-19 00:22:10 +0530

    COLLEGE LIBRARY MANAGEMENT

---
+++ Frontend/firebase.js
@@ -0,0 +1,15 @@
+var app_fireBase = {};
+(function () {
+  const firebaseConfig = {
+    apiKey: "AIzaSyByN3d5-ZL1ous3otZ_X6GJdpfEs-9liG4",
+    authDomain: "librarymanagement-6fc54.firebaseapp.com",
+    projectId: "librarymanagement-6fc54",
+    storageBucket: "librarymanagement-6fc54.appspot.com",
+    messagingSenderId: "1043464965637",
+    appId: "1:1043464965637:web:3e0294ee6f544bab83566c",
+    measurementId: "G-Y303RQEP2W"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  app_fireBase = firebase;
+})();
